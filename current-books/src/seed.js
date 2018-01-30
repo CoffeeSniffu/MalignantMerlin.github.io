214d52983a76e6fce60493bcc7b145c24a62e7b4
malignantmerlin <efujii6@gmail.com>
2018-01-30 02:04:37 -0500

    Update seed.js

--- current-books/src/seed.js
+++ current-books/src/seed.js
@@ -10,26 +10,26 @@ window.Seed = (function () {
         description: 'Geoff Johns -writer, Gary Frank -illustrator.',
         url: '#',
         votes: generateVoteCount(),
-        submitterAvatarUrl: 'src/images/avatars/daniel.jpg',
+        <!-- submitterAvatarUrl: 'src/images/avatars/daniel.jpg', -->
         productImageUrl: 'src/images/products/doomsdayclock.jpg',
       },
       {
         id: 2,
-        title: 'Supermajority: The Fantasy Congress League',
-        description: 'Earn points when your favorite politicians pass legislation.',
+        title: 'Raven Daughter Of Darkness',
+        description: 'Marv Wolfman -writer, Pop Mhan -illustrator.',
         url: '#',
         votes: generateVoteCount(),
-        submitterAvatarUrl: 'src/images/avatars/kristy.png',
-        productImageUrl: 'src/images/products/image-rose.png',
+        <!-- submitterAvatarUrl: 'src/images/avatars/kristy.png', -->
+        productImageUrl: 'src/images/products/ravendaughterofdarkness.jpg',
       },
       {
         id: 3,
-        title: 'Tinfoild: Tailored tinfoil hats',
-        description: 'We already have your measurements and shipping address.',
+        title: 'Darth Vader',
+        description: 'Charles Soule -writer, Giusuppe Camuncoli -illustrator.',
         url: '#',
         votes: generateVoteCount(),
-        submitterAvatarUrl: 'src/images/avatars/veronika.jpg',
-        productImageUrl: 'src/images/products/image-steel.png',
+        <!-- submitterAvatarUrl: 'src/images/avatars/veronika.jpg', -->
+        productImageUrl: 'src/images/products/darthvader.jpg',
       },
       {
         id: 4,
@@ -37,7 +37,7 @@ window.Seed = (function () {
         description: 'Andrew Constant -writer, Brad Walker -illustrator.',
         url: '#',
         votes: generateVoteCount(),
-        submitterAvatarUrl: 'src/images/avatars/molly.png',
+        <!-- submitterAvatarUrl: 'src/images/avatars/molly.png', -->
         productImageUrl: 'src/images/products/thedemonhellisearth.jpg',
       },
     ];
